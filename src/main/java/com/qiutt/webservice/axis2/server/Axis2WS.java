fb4b0d381aefc49483a6a80afaf71f579bf26ac6
DropOfJupiter <644570501@qq.com>
2017-04-21 15:36:12 +0800

    添加log4j

--- src/main/java/com/qiutt/webservice/axis2/server/Axis2WS.java
+++ src/main/java/com/qiutt/webservice/axis2/server/Axis2WS.java
@@ -1,5 +1,8 @@
 package com.qiutt.webservice.axis2.server;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * 使用AXIS2来实现webservice
  * 
@@ -7,7 +10,13 @@ package com.qiutt.webservice.axis2.server;
  *
  */
 public class Axis2WS {
+	//推荐使用commons-logging结合log4j进行日志记录
+	private static Log logger = LogFactory.getLog(Axis2WS.class);
 	public String byAxis2(String name) {
+		logger.error("Error:进入byAxis2方法啦");
+		logger.warn("Warn:进入byAxis2方法啦");
+		logger.info("Info:进入byAxis2方法啦");
+		logger.debug("Debug:进入byAxis2方法啦");
 		return name + "this is byAxis2";
 	}
 
